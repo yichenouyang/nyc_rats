de3cb904356f909d5ef3575288b492e98734c5a3
yichenouyang <yo2286@columbia.edu>
2018-10-26 12:59:33 -0400

    initial import3

--- map.js
+++ map.js
@@ -34,5 +34,48 @@ let geolocate = new mapboxgl.GeolocateControl({
 map.addControl(geolocate, 'top-left')
 
 geolocate.on('geolocate', function(event) {
+    // get the rat-sightings from the layer data
+    let features = map.queryRenderedFeatures({ layers: ['rat-sightings'] })
+    console.log(features)
+
+    // get the location of the click
+    let current_location = [event.coords.longitude, event.coords.latitude]
+
+    // if there aren't any features, don't continue
+    if (features.length == 0) return
+
+    // create variables to hold the closest feature found so far
+    let closest_distance = Infinity
+    let closest_feature = null
+
+    // we're going to check each feature
+    for (let feature of features) {
+
+        // calculate the distance using turf
+        let distance = turf.distance(turf.point(feature.geometry.coordinates), turf.point(current_location))
+
+        // if the distance is less than the closest distance we've seen so far, update the variables
+        if (distance < closest_distance) {
+            closest_distance = distance
+            closest_feature = feature
+        }        
+
+    }
+
+    // closest_distance should now be set to the minimum value
+    // closest_feature should be set to the feature itself
+    console.log("Closest feature:", closest_feature.geometry.coordinates, "(", closest_distance, "m)")
+
+    
+     // calculate bearing
+    let bearing = turf.bearing(turf.point(current_location), turf.point(closest_feature.geometry.coordinates))
+    console.log("Bearing:", bearing)
+    // additional handler code goes here
+    
+    // turn the pointer in that direction
+    var pointer = document.getElementById('pointer')
+    pointer.style.transform = 'rotate(' + bearing + 'deg)'
+
+    map.flyTo({ center: current_location })
 
 })
